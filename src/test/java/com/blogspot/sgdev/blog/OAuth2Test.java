d0b3e58a47eb07648b1926dca56ed37af2cca019
Nils Bokermann <nils.bokermann@bermuda.de>
2018-11-26 16:02:59 +0100

    Catch JSONException when builing new Object.

--- src/test/java/com/blogspot/sgdev/blog/OAuth2Test.java
+++ src/test/java/com/blogspot/sgdev/blog/OAuth2Test.java
@@ -4,37 +4,46 @@ import org.jose4j.json.JsonUtil;
 import org.jose4j.jwt.consumer.JwtConsumer;
 import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 import org.jose4j.jwt.consumer.JwtContext;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
  * @author thomas.tesche
  */
 public class OAuth2Test {
 
-   protected JwtConsumer jwtConsumer;
+    protected static final Logger logger = LoggerFactory.getLogger(OAuth2Test.class);
+    protected JwtConsumer jwtConsumer;
 
-   protected static final Logger logger = LoggerFactory.getLogger(OAuth2Test.class);
+    @Before
+    public void setup() {
+        jwtConsumer = new JwtConsumerBuilder().setSkipAllValidators().setDisableRequireSignature()
+                                              .setSkipSignatureVerification()
+                                              .build();
+    }
 
-   @Before
-   public void setup() {
-      jwtConsumer = new JwtConsumerBuilder().setSkipAllValidators().setDisableRequireSignature().setSkipSignatureVerification()
-         .build();
-   }
+    protected void logJWTClaims(JwtContext jwtContext) {
+        try {
+            logger.info(prettyPrintJson(JsonUtil.toJson(jwtContext.getJwtClaims().getClaimsMap())));
+        } catch (JSONException e) {
+            logger.warn("JSON Object not parsable");
+        }
+    }
 
-   protected void logJWTClaims(JwtContext jwtContext) {
-      logger.info(prettyPrintJson(JsonUtil.toJson(jwtContext.getJwtClaims().getClaimsMap())));
-   }
+    protected void logJson(String json) {
 
-   protected void logJson(String json) {
-      logger.info(prettyPrintJson(json));
-   }
+        try {
+            logger.info(prettyPrintJson(json));
+        } catch (JSONException e) {
+            logger.warn("JSON Object not parsable");
+        }
+    }
 
-   protected String prettyPrintJson(String flatJson) {
-      return (new JSONObject(flatJson).toString(3));
-   }
+    protected String prettyPrintJson(String flatJson) throws JSONException {
+        return (new JSONObject(flatJson).toString(3));
+    }
 
 }
